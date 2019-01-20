46c776360acf2c47813f66a1b1afc765faa090ce
petertran198 <petertran198@gmail.com>
2019-01-20 15:49:02 -0500

    Implemented partial for nav and footer in application layout

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,5 +3,6 @@ class Course < ApplicationRecord
   validates :short_name, presence: true, length: {minimum: 5, maximum: 20}
   validates :description, presence: true, length: {minimum: 10, maximum: 300}
 
+  
 
 end
