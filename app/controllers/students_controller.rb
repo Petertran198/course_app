fa92b2d7939a9b14234e59d2134c48e9a6c52f38
petertran198 <petertran198@gmail.com>
2019-01-20 21:35:00 -0500

    Implemented best pratice DRY my code in student controller, and made partial for form and partial for errors

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,5 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: [:edit, :update, :show]
 
   def index
     @students = Student.all
@@ -12,22 +13,22 @@ class StudentsController < ApplicationController
     @student = Student.new(student_params)
     if @student.save
       flash[:notice] = "You have successfully signed up"
-      redirect_to root_path
+      redirect_to student_path(@student)
     else
       render 'new'
     end
   end
 
   def show
-    @student = Student.find(params[:id])
+
   end
 
   def edit
-    @student = Student.find(params[:id])
+    
   end
 
   def update
-    @student = Student.find(params[:id])
+ 
     if @student.update(student_params)
       flash[:notice] = "You have successfully updated your profile"
       redirect_to student_path(@student)
@@ -47,6 +48,9 @@ class StudentsController < ApplicationController
     params.require(:student).permit(:name, :email)
   end
 
+  def set_student
+     @student = Student.find(params[:id])
+  end
 
 
 
